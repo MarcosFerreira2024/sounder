e3fdf662220f0c669994934187e09ca02d21ed9f
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-01-09 09:36:12 -0300

    Add CategoriesCarousel component

---
+++ frontend/src/components/CategoriesCarousel.tsx
@@ -0,0 +1,69 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { useTooltip } from "../contexts/TooltipContext";
+import useCategories from "../hooks/useCategories";
+
+function CategoriesCarousel() {
+  const {
+    canScrollLeft,
+    canScrollRight,
+    categories,
+    containerRef,
+    containerSize,
+    scrollLeft,
+    scrollRight,
+    updateScrollButtons,
+    gap,
+    itemSize,
+  } = useCategories();
+
+  const navigate = useNavigate();
+
+  const { hideTooltip, showTooltip } = useTooltip();
+
+  return (
+    <div className="flex items-center gap-2">
+      <ChevronLeft
+        className={`${!canScrollLeft && "opacity-50"} text-neutral-400`}
+        onClick={() => scrollLeft()}
+      />
+
+      <div
+        ref={containerRef}
+        onScroll={updateScrollButtons}
+        style={{
+          gap,
+          maxWidth: containerSize,
+        }}
+        className="flex items-center overflow-x-auto scrollbar-hide"
+      >
+        {categories.map((item, index) => (
+          <button
+            onMouseLeave={hideTooltip}
+            onMouseMove={(e) => showTooltip(e, item.name)}
+            onClick={() => navigate(item.to)}
+            key={index}
+            className=" bg-neutral-700 rounded-lg "
+          >
+            <img
+              style={{
+                minWidth: itemSize,
+                minHeight: itemSize,
+                maxHeight: itemSize,
+                maxWidth: itemSize,
+              }}
+              src={item.photo}
+              className="   object-cover rounded-lg border border-neutral-900"
+            />
+          </button>
+        ))}
+      </div>
+      <ChevronRight
+        className={`${!canScrollRight && "opacity-50"} text-neutral-400`}
+        onClick={() => scrollRight()}
+      />
+    </div>
+  );
+}
+
+export default CategoriesCarousel;
