83a2fe3c34127b32e83c40484bed4040b3d282e8
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-01-09 09:39:49 -0300

    Add useAuthCarousel hook for auth image carousel

---
+++ frontend/src/hooks/useAuthCarousel.ts
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react'
+
+function useAuthCarousel(isTyping:boolean) {
+
+  const images = [
+    { alt: "Tyler The Creator, Flower Boy", src: "auth/flower-boy.webp" },
+    { alt: "SZA, SOS", src: "auth/sza-sos.webp" },
+    { alt: "The Weekend, After Hours", src: "auth/after-hours.webp" },
+  ];
+  const minIndex = 0;
+  const maxIndex = images.length - 1;
+  const interval = 3000; // 3s 
+
+  type scrollPreference = "paused" | "running";
+
+  const [currentIndex, setCurrentIndex] = useState(minIndex);
+  const [autoScrollPreference, setAutoScrollPreference] =
+    useState<scrollPreference>(() => {
+      const savedPreference = localStorage.getItem(
+        "autoScrollPreference"
+      ) as scrollPreference;
+      return savedPreference || "running";
+    });
+
+  useEffect(() => {
+    localStorage.setItem("autoScrollPreference", autoScrollPreference);
+  }, [autoScrollPreference]);
+
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleScrollPreference = () => {
+    if (autoScrollPreference === "running")
+      return setAutoScrollPreference("paused");
+    else return setAutoScrollPreference("running");
+  };
+
+  useEffect(() => {
+    if (isHovering) return;
+    if (isTyping) return;
+    if (autoScrollPreference === "paused") return;
+
+    const timer = setInterval(() => {
+      setCurrentIndex((prev) => (prev === maxIndex ? minIndex : prev + 1));
+    }, interval);
+
+    return () => clearInterval(timer);
+  }, [isHovering]);
+
+
+  return {
+    images,
+    isHovering,
+    isTyping,
+    handleScrollPreference,
+    setIsHovering,
+    currentIndex,
+    minIndex,
+    maxIndex,
+    autoScrollPreference,
+    setCurrentIndex,
+
+
+  }
+}
+
+export default useAuthCarousel
