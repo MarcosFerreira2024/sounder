8ec3589742c911083e5732c422213e498559061f
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-01-14 16:06:30 -0300

    Update AuthLayout.tsx

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AuthCarousel from "../components/AuthCarousel";
+import AuthCarousel from "../components/auth/AuthCarousel";
 import { Link, useNavigate } from "react-router-dom";
 
 type AuthLayoutProps = {
