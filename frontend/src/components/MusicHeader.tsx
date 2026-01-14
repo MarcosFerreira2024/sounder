5ba24bdf9cbe970b48f4b96d2f4f82a1aa529971
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-01-14 15:50:30 -0300

    Refactor MusicHeader to use MediaInfoHeader component

--- frontend/src/components/MusicHeader.tsx
+++ frontend/src/components/MusicHeader.tsx
@@ -1,25 +1,17 @@
 import { Heart } from "lucide-react";
-import Button from "./Button";
+import Button from "./ui/Button";
 import { useAudioContext } from "../contexts/AudioContext";
+import { MediaInfoHeader } from "./MediaInfoHeader";
 
 export function MusicHeader() {
-  const { author_photo, music_name, author_name } = useAudioContext();
+  const { currentMusic } = useAudioContext();
+
+  const image = currentMusic?.photo ?? "/music-cover-mock.png";
+  const title = currentMusic?.name ?? "No music selected";
+
   return (
-    <div className=" py-2 flex justify-between items-center px-4  bg-neutral-950 w-full rounded-2xl">
-      <div className="flex items-start gap-2 ">
-        <img
-          src={author_photo}
-          className="rounded-full border border-neutral-900 w-18 h-18"
-          alt={author_name}
-        />
-        <div className="flex flex-col  font-inter">
-          <h1 className="text-neutral-100 text-2xl">{music_name}</h1>
-          <p className="text-neutral-400 text-base">{author_name}</p>
-        </div>
-      </div>
-      <div>
-        <Button title="Like" icon={<Heart />} roundedValue="full" size="md" />
-      </div>
-    </div>
+    <MediaInfoHeader subtitle={"teste"} image={image} title={title}>
+      <Button title="Like" icon={<Heart />} roundedValue="full" size="md" />
+    </MediaInfoHeader>
   );
 }
