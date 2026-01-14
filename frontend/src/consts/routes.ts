254e492bef8771702a309ddf935f4efdc29b5464
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-01-14 16:00:04 -0300

    Creating a const to store all the backend routes used on fetchs

---
+++ frontend/src/consts/routes.ts
@@ -0,0 +1,10 @@
+export const BACKEND_API_URL = "http://localhost:3000"
+
+export const routes = {
+  auth: {
+    signup: `${BACKEND_API_URL}/signup`,
+    login: `${BACKEND_API_URL}/login`,
+  },
+
+
+} 
