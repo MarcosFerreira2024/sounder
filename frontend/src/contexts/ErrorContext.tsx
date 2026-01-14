1b7be92386d296eb6ef7615542a5bf62f8768287
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-01-14 15:59:06 -0300

    Creating a context to handle all the app Errors

---
+++ frontend/src/contexts/ErrorContext.tsx
@@ -0,0 +1,46 @@
+import { createContext, useContext } from "react";
+import { useError } from "../hooks/useError";
+import AppError from "../components/ui/AppError";
+
+type AppErrorContextType = ReturnType<typeof useError>;
+
+const AppErrorContext = createContext<AppErrorContextType | null>(null);
+
+type AppErrorProviderProps = {
+  children: React.ReactNode;
+};
+
+export function AppErrorProvider({ children }: AppErrorProviderProps) {
+  const { clearError, error, setError, setIsHovering, handleAppErrors } =
+    useError();
+
+  return (
+    <AppErrorContext.Provider
+      value={{
+        clearError,
+        error,
+        handleAppErrors,
+        setError,
+        setIsHovering,
+      }}
+    >
+      {children}
+
+      <AppError
+        setIsHovering={setIsHovering}
+        message={error}
+        onClose={clearError}
+      />
+    </AppErrorContext.Provider>
+  );
+}
+
+export function useAppError() {
+  const context = useContext(AppErrorContext);
+
+  if (!context) {
+    throw new Error("useAppError must be used within an AppErrorProvider");
+  }
+
+  return context;
+}
