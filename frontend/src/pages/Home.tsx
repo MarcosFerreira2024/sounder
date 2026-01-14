90e440e7af2341fd592079e14f8c7d794439209e
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-01-14 16:06:03 -0300

    Improving home page component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import MainLayout from "../layouts/MainLayout";
 import PlayerLayout from "../layouts/PlayerLayout";
-import Sidebar from "../components/Sidebar";
 import { useState } from "react";
 import { SidePanel } from "../components/SidePanel";
 
@@ -14,15 +13,13 @@ function Home() {
 
   return (
     <MainLayout>
-      <div className="text-neutral-100 grid gap-4">
-        <PlayerLayout
-          toggleLyricsVisibilty={toggleLyricsVisibilty}
-          isLyricsVisible={isVisible}
-        >
-          <SidePanel lyricsVisible={isVisible} showPlaylistMusic={false} />
-        </PlayerLayout>
-        <Sidebar />
-      </div>
+      <PlayerLayout
+        toggleLyricsVisibilty={toggleLyricsVisibilty}
+        isLyricsVisible={isVisible}
+        showExtraControls={true}
+      >
+        <SidePanel lyricsVisible={isVisible} showPlaylistMusic={false} />
+      </PlayerLayout>
     </MainLayout>
   );
 }
