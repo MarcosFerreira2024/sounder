c2a701e30d94b13af401a470898ad7ff59792fb8
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-01-14 15:49:07 -0300

    Show message when lyrics are unavailable
    ated

--- frontend/src/components/Lyrics.tsx
+++ frontend/src/components/Lyrics.tsx
@@ -1,6 +1,6 @@
 import { useLyrics } from "../hooks/useLyrics";
 import { AnimatePresence } from "framer-motion";
-import SyncButton from "./SyncButton";
+import SyncButton from "./ui/SyncButton";
 import { useAudioContext } from "../contexts/AudioContext";
 
 export function Lyrics() {
@@ -24,27 +24,33 @@ export function Lyrics() {
           isManualScrolling={isManualScrolling}
         />
       </AnimatePresence>
-      <div className="  bg-neutral-950 border-neutral-900  overflow-y-scroll min-h-full   border rounded-2xl  flex flex-col">
-        <div ref={scrollContainerRef} className="grid gap-10 p-4 ">
-          {parsedLyrics.map((line, index) => {
-            const isActive = index === activeIndex;
-            return (
-              <h1
-                key={index}
-                ref={(el) => {
-                  lineRefs.current[index] = el;
-                }}
-                onClick={() => handleLineClick(line.timeInMs)}
-                className={`font-inter text-4xl w-fit  ${
-                  isActive
-                    ? "text-neutral-100"
-                    : "text-neutral-400 hover:text-neutral-100 hover:underline hover:decoration-2  hover:decoration-neutral-100 hover:underline-offset-4"
-                }`}
-              >
-                {line.text}
-              </h1>
-            );
-          })}
+      <div className=" min-h-[620px]  bg-neutral-950 border-neutral-900  overflow-y-scroll   border rounded-2xl  grid">
+        <div ref={scrollContainerRef} className="grid  gap-10 p-4  ">
+          {parsedLyrics.length === 0 ? (
+            <p className="text-opacity text-center w-full h-full text-lg">
+              Essa música ainda não possui letra.
+            </p>
+          ) : (
+            parsedLyrics.map((line, index) => {
+              const isActive = index === activeIndex;
+              return (
+                <h1
+                  key={index}
+                  ref={(el) => {
+                    lineRefs.current[index] = el;
+                  }}
+                  onClick={() => handleLineClick(line.timeInMs)}
+                  className={`font-inter text-4xl w-fit  ${
+                    isActive
+                      ? "text-neutral-100"
+                      : "text-neutral-400 hover:text-neutral-100 hover:underline hover:decoration-2  hover:decoration-neutral-100 hover:underline-offset-4"
+                  }`}
+                >
+                  {line.text}
+                </h1>
+              );
+            })
+          )}
         </div>
       </div>
     </>
