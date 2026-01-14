bf4594d14861891add640fb0fb60f7ca9a784560
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-01-14 16:06:26 -0300

    Update MainLayout.tsx

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import Header from "../components/Header";
+import Header from "../components/ui/Header";
+import Sidebar from "../components/ui/Sidebar"; // Import Sidebar
 
 function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-neutral-950 h-screen pb-4 px-4">
-      <div className=" mx-auto w-full  flex flex-col">
+    <div className="bg-neutral-950 h-screen">
+      <div className="mx-auto w-full flex flex-col h-[calc(100dvh - 16px)] pt-2 pb-2 px-4">
         <Header />
-        <div className=" h-full">{children}</div>
+        <div style={{ minHeight: "calc(100dvh - 84px - 100px)" }}>
+          {children}
+        </div>
+        <Sidebar />
       </div>
     </div>
   );
