f3a41cc1c60b7bc908bc6fccb2db7b896131625b
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-01-21 20:39:00 -0300

    Interface for userRepo

---
+++ backend/src/modules/user/interfaces/IUserRepository.ts
@@ -0,0 +1,17 @@
+
+import { User } from "../../../generated/prisma/client";
+ interface IUserRepository {
+  findById(id: string): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
+  findByName(name: string): Promise<User[]>;
+  findAll(page: number, limit: number): Promise<User[]>;
+
+
+  update(id: string,data: Partial<{  name: string;  image: string; }>
+  ): Promise<User>;
+
+  delete(id: string): Promise<void>;
+}
+
+
+export { IUserRepository }
