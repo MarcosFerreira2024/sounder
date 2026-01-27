78e12be0fde91c7594f6b092b0140356a257c026
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-01-27 20:40:46 -0300

    Adding new singletons to tsyringe

--- backend/src/shared/container.ts
+++ backend/src/shared/container.ts
@@ -2,7 +2,15 @@ import { container } from "tsyringe";
 import { IUserRepository } from "../modules/user/interfaces/IUserRepository";
 import { UserRepository } from "../modules/user/repos/UserRepository";
 import { IFollowRepository } from "../modules/follow/interfaces/IFollowRepository";
-import { FollowRepository } from "../modules/follow/repo/FollowRepository";
+import { FollowRepository } from "../modules/follow/repos/FollowRepository";
+import { IPlaylistRepository } from "../modules/playlist/interfaces/IPlaylistRepository";
+import { PlaylistRepository } from "../modules/playlist/repos/PlaylistRepository";
+import { IMusicRepository } from "../modules/music/interfaces/IMusicRepository";
+import { MusicRepository } from "../modules/music/repos/MusicRepository";
+import { IMusicActionRepository } from "../modules/music/interfaces/IMusicActionRepository";
+import { MusicActionRepository } from "../modules/music/repos/MusicActionRepository";
+import { IAlbumRepository } from "../modules/album/interfaces/IAlbumRepository";
+import { AlbumRepository } from "../modules/album/repos/AlbumRepository";
 
 container.registerSingleton<IUserRepository>(
   "UserRepository",
@@ -13,3 +21,16 @@ container.registerSingleton<IFollowRepository>(
   "FollowRepository",
   FollowRepository
 )
+
+
+container.registerSingleton<IPlaylistRepository>(
+  "PlaylistRepository",
+  PlaylistRepository
+);
+
+
+container.registerSingleton<IMusicRepository>("MusicRepository", MusicRepository)
+
+container.registerSingleton<IMusicActionRepository>("MusicActionRepository", MusicActionRepository)
+
+container.registerSingleton<IAlbumRepository>("AlbumRepository", AlbumRepository)
