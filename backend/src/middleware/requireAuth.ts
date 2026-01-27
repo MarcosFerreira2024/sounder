6fca5f8e83e4efe1204c302c2ffdce923711a540
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-01-27 20:15:56 -0300

    Update requireAuth.ts

--- backend/src/middleware/requireAuth.ts
+++ backend/src/middleware/requireAuth.ts
@@ -1,10 +1,11 @@
-import { Session, User } from "better-auth/types";
 import { NextFunction, Request, Response } from "express";
+import { AppUser } from "../shared/types/user";
+import { Session } from "better-auth/types";
 
 declare global {
     namespace Express {
         interface Request {
-            user?: User;
+            user?: AppUser;
             session?: Session;
         }
     }
