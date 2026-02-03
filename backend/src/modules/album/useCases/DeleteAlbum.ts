49458e687e2e778089a82b447dd54e3d9a545a78
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 02:59:28 -0300

    Update DeleteAlbum.ts

--- backend/src/modules/album/useCases/DeleteAlbum.ts
+++ backend/src/modules/album/useCases/DeleteAlbum.ts
@@ -2,14 +2,17 @@ import { injectable, inject } from "tsyringe";
 import { IAlbumRepository } from "../interfaces/IAlbumRepository";
 import { canChangeAlbum } from "../rules/canChangeAlbum";
 import { AppUser } from "../../../shared/types/user";
+import { IMusicRepository } from "../../music/interfaces/IMusicRepository";
 
 @injectable()
 class DeleteAlbum {
   constructor(
-    @inject("AlbumRepository") private albumRepository: IAlbumRepository
+    @inject("AlbumRepository") private albumRepository: IAlbumRepository,
+    @inject("MusicRepository") private musicRepository: IMusicRepository
   ) {}
 
   async execute(user:AppUser,id: string): Promise<void> {
+
 
 
 
@@ -19,8 +22,10 @@ class DeleteAlbum {
 
     if (!albumFound) throw new Error("Album not found");
 
-    canChangeAlbum(user, id);
+    if(!canChangeAlbum(user, id)) throw new Error("You don't have permission to delete this album");
 
+
+    const musics = await this.musicRepository.updateMany(id, { albumId: null });
 
     await this.albumRepository.delete(id);
   }
