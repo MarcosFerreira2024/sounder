314aaffa8c87f681f5728b7b10de044929da703e
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 03:07:21 -0300

    Add create gamemode implementation

---
+++ backend/src/modules/game/useCases/CreateGamemode.ts
@@ -0,0 +1,28 @@
+import { inject, injectable } from "tsyringe";
+import { IGameModesRepository } from "../interfaces/IGameModesRepository";
+import { GameMode } from "../../../generated/prisma/client";
+
+
+@injectable()
+class CreateGamemode {
+
+    constructor(
+        @inject("GameModesRepository") private gameModeRepository: IGameModesRepository
+    ) {}
+
+
+    async execute(name:string, description:string): Promise<GameMode> {
+
+       const exists = await this.gameModeRepository.getGameModes({name})
+        if(exists.length > 0) return exists[0]
+
+
+       const gameMode = await this.gameModeRepository.create(name,description)
+
+       return gameMode
+    }
+
+
+}
+
+export { CreateGamemode }
