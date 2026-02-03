e94233d17a4391d806503f2048a95da8640631b5
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 03:12:45 -0300

    Update user routes with profile picture upload

--- backend/src/modules/user/routes/route.ts
+++ backend/src/modules/user/routes/route.ts
@@ -3,22 +3,35 @@ import { UserController } from "../controllers/UserController";
 import { deserializeUser } from "../../../middleware/deserializeUser";
 import { requireAuth } from "../../../middleware/requireAuth";
 import { validate } from "../../../middleware/validateSchema";
-import { userId } from "../../../shared/schema/schema";
+import { upload } from "../../../libs/multer";
+import { uploadWithErrorHandler } from "../../../middleware/uploadWithErrorHandler";
+import { optionalId, userUpdateBody } from "../schemas/schema";
 
 export function userRoutes(): Router {
     const router = Router();
 
     const userController = new UserController();
 
+
+
+    router.get("/:userId",deserializeUser,requireAuth, validate({ params: optionalId}),   userController.getUser);
+
     router.get("/", deserializeUser, requireAuth,(req: Request, res: Response) =>  {
         res.json(req.user);
     });
 
-    router.get("/:userId", validate({ params: userId }),   userController.getUser);
 
-    router.delete("/:userId",deserializeUser,requireAuth, validate({ params: userId }),   userController.delete);
+    router.delete("/:userId",deserializeUser,requireAuth, validate({ params: optionalId}),   userController.delete);
 
-    router.patch("/:userId",deserializeUser,requireAuth, validate({ params: userId }),   userController.update);
+    router.put("/:userId",deserializeUser,requireAuth, validate({ params: optionalId, body: userUpdateBody}),   userController.update);
+
+    router.post("/profilePicture",
+        deserializeUser,
+        requireAuth,
+        uploadWithErrorHandler(upload.single("image")),
+        validate({ body: optionalId}),
+        userController.changeProfilePicture
+    );
 
     return router;
 }
