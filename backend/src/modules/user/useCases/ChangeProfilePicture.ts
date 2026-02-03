abda04d59df3d94e89c893097fe198824843abb5
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 03:12:54 -0300

    Add ChangeProfilePicture use case

---
+++ backend/src/modules/user/useCases/ChangeProfilePicture.ts
@@ -0,0 +1,63 @@
+import { inject, injectable } from "tsyringe"
+import { IUserRepository } from "../interfaces/IUserRepository"
+import { isAdmin } from "../../../shared/rules/isAdmin"
+import { User } from "../../../generated/prisma/client"
+import { AppUser } from "../../../shared/types/user"
+import { IFileStorage } from "../../../shared/storage/IFileStorage"
+
+type ChangeProfilePictureDTO = {
+  user: AppUser
+  image: {
+    buffer: Buffer
+    originalName: string
+    mimeType: string
+  }
+  userId?: string
+}
+
+@injectable()
+class ChangeProfilePicture {
+  constructor(
+    @inject("UserRepository")
+    private userRepository: IUserRepository,
+    @inject("FileStorage")
+    private fileStorage: IFileStorage
+  ) {}
+
+  async execute({
+    user,
+    image,
+    userId
+  }: ChangeProfilePictureDTO): Promise<User> {
+
+    let targetUserId: string
+
+    
+
+
+
+
+    if (userId) {
+      if (!isAdmin(user)) throw new Error("You don't have permission to change another user's profile picture")
+      const userExists = await this.userRepository.findById(userId)
+      if (!userExists) throw new Error("User not found")
+
+      targetUserId = userId
+    } else {
+      targetUserId = user.id
+    }
+
+    const { path } = await this.fileStorage.save({
+      buffer: image.buffer,
+      filename: image.originalName,
+      folder: `${targetUserId}/profile-pictures`
+    })
+
+    return await this.userRepository.changeProfilePicture(
+      targetUserId,
+      path
+    )
+  }
+}
+
+export { ChangeProfilePicture }
