ea1dec3a764d9d9abe35a2767e35f5ee3758e554
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 03:00:46 -0300

    Add ArtistController with assign and find methods

---
+++ backend/src/modules/artist/controllers/ArtistController.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { AssignUserAsArtist } from "../useCases/AssignUserAsArtist";
+import { handleAppError } from "../../../shared/helpers/handleAppError";
+import { normalizePagination } from "../../../shared/helpers/normalizePagination";
+import { FindArtists } from "../useCases/FindArtist";
+
+class ArtistController {
+
+    async assignUserAsArtist(req: Request, res: Response) {
+
+        try {
+
+
+            await container.resolve(AssignUserAsArtist).execute(req.user!,req.params.userId as string)
+            
+
+            return res.status(201).json({message:"Artist Assigned successfully"})
+
+
+        }
+
+        catch(e)
+        {
+
+            return handleAppError(res,e)
+
+        }
+
+
+    }
+
+    async findArtists(req: Request, res: Response) {
+
+        try {
+            const { limit, page } = normalizePagination(
+                req.query.page as unknown as number,
+                req.query.limit as unknown as number,
+                req.user!
+            );
+
+            const search = {
+                    name: req.query.name as string | undefined,
+                    id: req.query.id as string | undefined,
+                    musicName: req.query.musicName as string | undefined,
+                    albumName: req.query.albumName as string | undefined,
+
+            }
+
+            const artists = await container.resolve(FindArtists).execute(search,page,limit)
+
+
+            return res.status(200).json({data:artists, message:"Artists fetched successfully"})
+        }
+
+        catch(e){
+            return handleAppError(res,e)
+        }
+    }
+
+
+}
+
+export { ArtistController };
