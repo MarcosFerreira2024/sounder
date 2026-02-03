1f555369a855b50c18c1c170dc9e68efe066b589
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 03:13:47 -0300

    Add normalizePaginatedResponse helper function

---
+++ backend/src/shared/helpers/normalizePaginatedResponse.ts
@@ -0,0 +1,8 @@
+function normalizePaginatedResponse<T>(items: T[], page: number | undefined) {
+  return {
+    items,
+    page: page ?? 1,
+    totalItems: items.length
+  }
+}
+export { normalizePaginatedResponse }
