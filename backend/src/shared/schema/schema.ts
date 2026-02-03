639a4f769646db0a7f832c8f901c4bb2794fd8f3
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 03:14:14 -0300

    Refactor schema error messages and add musicIdParam

--- backend/src/shared/schema/schema.ts
+++ backend/src/shared/schema/schema.ts
@@ -1,4 +1,6 @@
 import z from "zod"
+import { zodErrorMessages } from "../constants/errors";
 
-    export const userId = z.uuid({error:"Invalid user ID, provide a valid user ID"}).min(1, "User ID is required")
-    export const playlistId = z.uuid({error:"Invalid playlist ID, provide a valid playlist ID"}).min(1, "Playlist ID is required")
+export const userId = z.uuid({error:zodErrorMessages.invalid("user ID")}).min(1, zodErrorMessages.required("User ID"))
+export const playlistId = z.uuid({error:zodErrorMessages.invalid("playlist ID")}).min(1, zodErrorMessages.required("Playlist ID"))
+export const musicIdParam = z.uuid({error:zodErrorMessages.invalid("music ID")}).min(1, zodErrorMessages.required("Music ID"))
