6fe97806c089ddb605011050b327eb30cb10b521
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 03:07:05 -0300

    Add CreateGame use case implementation

---
+++ backend/src/modules/game/useCases/CreateGame.ts
@@ -0,0 +1,29 @@
+import { inject, injectable } from "tsyringe";
+import { Game } from "../../../generated/prisma/client";
+import { IGameRepository } from "../interfaces/IGameRepository";
+
+
+@injectable()
+class CreateGame {
+
+
+    constructor(@inject("GameRepository") private gameRepository: IGameRepository) {}
+
+
+
+
+    async execute(dailyGameId: string, gamemodeId: string): Promise<Game> {
+
+        const game = await this.gameRepository.findByDailyId(dailyGameId)
+        if(game) throw new Error("Game already created")
+
+
+
+        return this.gameRepository.create(dailyGameId, gamemodeId)
+    }
+
+
+
+}
+
+export { CreateGame }
