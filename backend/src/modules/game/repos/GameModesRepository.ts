04e62170c2eb25d3a8f3b18ae8e7a4cd94194bdb
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 03:05:13 -0300

    Create GameModesRepository.ts

---
+++ backend/src/modules/game/repos/GameModesRepository.ts
@@ -0,0 +1,56 @@
+import { GameMode } from "../../../generated/prisma/client";
+import { prisma } from "../../../libs/prismaClient";
+import { gameModesQueryFilters, IGameModesRepository } from "../interfaces/IGameModesRepository";
+
+class GameModesRepository implements IGameModesRepository {
+
+
+
+    async create(name: string, description: string): Promise<GameMode> {
+        
+       return await prisma.gameMode.create({
+            data: {
+                name,
+                description
+            }
+        })
+
+
+    }
+    async delete(id: string): Promise<void> {
+        await prisma.gameMode.delete({where: {id}})
+    }
+
+    async getGameModes( search?: gameModesQueryFilters,page?: number, limit?: number): Promise<GameMode[]> {
+
+
+        return await prisma.gameMode.findMany({
+            where: {
+                  ...(search?.name &&  { name: { contains: search.name } }),
+                  ...(search?.id &&  { id: search.id }),
+                  ...(search?.description &&  { description: { contains: search.description } }),
+            },
+            take: limit && limit,
+            skip: page && limit && (page - 1) * limit,
+            select: {
+                id: true,
+                name: true,
+                description: true
+            }
+        })
+
+    }
+
+    async update(id: string, data: { name?: string; description?: string; }): Promise<GameMode> {
+        
+        return await prisma.gameMode.update({
+            where: {
+                id
+            },
+            data
+        })
+
+    }
+}
+
+export { GameModesRepository }
