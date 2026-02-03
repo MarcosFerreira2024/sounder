9db92f44675015909c20760fe57e8faffb8e13bc
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 03:08:11 -0300

    Add GetUserGameStats use case implementation

---
+++ backend/src/modules/game/useCases/GetUserGameStats.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from "tsyringe";
+import { IGameUserRepository } from "../interfaces/IGameUserRepository";
+import { AppUser } from "../../../shared/types/user";
+import { IGameModesRepository } from "../interfaces/IGameModesRepository";
+
+
+@injectable()
+
+class GetUserGameStats {
+
+      constructor(@inject("GameUserRepository") private gameUserRepository: IGameUserRepository, @inject("GameModesRepository") private gameModesRepository: IGameModesRepository) {}
+
+
+      async execute(user:AppUser,gameMode: string){
+
+        const mode = await this.gameModesRepository.getGameModes({name: gameMode})
+        const id = mode[0].id
+        
+        if(!id) throw new Error("Game mode not found")
+
+        const stats =await this.gameUserRepository.getUserStats(user.id,id)
+
+        if(stats) return stats
+
+        return await this.gameUserRepository.createStats(user.id,id)
+
+      }
+}
+
+export { GetUserGameStats }
