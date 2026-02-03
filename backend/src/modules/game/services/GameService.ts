f35f504c0416ce40f286a2fa4550ce34d9817639
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 03:06:26 -0300

    Creating a game service to handle multiple game modes

---
+++ backend/src/modules/game/services/GameService.ts
@@ -0,0 +1,28 @@
+import { container, injectable } from "tsyringe";
+import { CreateDailyGame } from "../useCases/CreateDailyGame";
+import { CreateGamemode } from "../useCases/CreateGamemode";
+import { CreateGame } from "../useCases/CreateGame";
+
+
+
+@injectable()
+class GameService{
+
+
+
+
+
+    async normalMode () {
+        const dailyGame = await container.resolve(CreateDailyGame).execute()
+        const mode = await container.resolve(CreateGamemode).execute("Normal","Good luck, you have 5 tries to guess the correct author of the song")
+        await container.resolve(CreateGame).execute(dailyGame.id, mode.id)
+
+        console.log("Normal mode created successfully")
+    }
+
+
+
+
+}
+
+export {GameService}
