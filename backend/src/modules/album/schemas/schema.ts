4a971e4633e4f4556d61e2d0c20aeccf7052c4d3
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 02:59:04 -0300

    Creating album schemas

---
+++ backend/src/modules/album/schemas/schema.ts
@@ -0,0 +1,12 @@
+import z from "zod";
+import { zodErrorMessages } from "../../../shared/constants/errors";
+
+export const albumId = z.object({
+  id: z.string({ error: zodErrorMessages.invalid("ID") }),
+}).strict();
+
+
+export const createAlbum = z.object({
+        name:z.string({error: zodErrorMessages.required("Album Name")}).min(1, zodErrorMessages.required("Album Name")),
+        artistId: z.uuid({error: zodErrorMessages.invalid("User ID")}).optional()
+    })
