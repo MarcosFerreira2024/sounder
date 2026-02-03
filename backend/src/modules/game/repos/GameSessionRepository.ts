ac91efdf6b4b26ea65f447a6da666c347fe9667c
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 03:05:22 -0300

    Create GameSessionRepository.ts

---
+++ backend/src/modules/game/repos/GameSessionRepository.ts
@@ -0,0 +1,58 @@
+import { connect } from "http2";
+import { prisma } from "../../../libs/prismaClient";
+import { GameSessionCreationArgs, GameSessionUpdate, IGameSessionRepository } from "../interfaces/IGameSessionRepository";
+import { GameSession } from "../../../generated/prisma/client";
+
+class GameSessionRepository implements IGameSessionRepository {
+    async createSession(data: GameSessionCreationArgs): Promise<GameSession> {
+
+
+        return await prisma.gameSession.create({
+            data: {
+                userId: data.userId,
+                gameId: data.gameId,
+                status: "IN_PROGRESS",
+                tries: 0
+            }
+        })
+        
+    }
+
+    async deleteSession(sessionId: string): Promise<void> {
+        await prisma.gameSession.delete({
+            where: {
+                id: sessionId
+            }
+        })
+    }
+
+    async findSession(gameId: string, userId: string): Promise<GameSession | null> {
+
+        const gameSession = await prisma.gameSession.findUnique({
+            where: {
+                userId_gameId: {
+                    gameId,
+                    userId
+                }
+            }
+        })
+
+        return gameSession
+
+        
+    }
+        
+    async updateSession(sessionId: string, data: Partial<GameSessionUpdate>): Promise<GameSession> {
+
+        return await prisma.gameSession.update({
+            where: {
+                id: sessionId
+            },
+            data
+        })
+        
+    }
+
+}
+
+export { GameSessionRepository }
