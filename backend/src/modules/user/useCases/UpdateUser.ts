177e977192ef8443d62482996480210578264fba
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 03:13:08 -0300

    Enhance UpdateUser to support admin updates and email checks

--- backend/src/modules/user/useCases/UpdateUser.ts
+++ backend/src/modules/user/useCases/UpdateUser.ts
@@ -1,6 +1,8 @@
 import { injectable, inject } from "tsyringe";
 import { IUserRepository } from "../interfaces/IUserRepository";
 import { User } from "better-auth/types";
+import { isAdmin } from "../../../shared/rules/isAdmin";
+import { AppUser } from "../../../shared/types/user";
 
 @injectable()
  class UpdateUser {
@@ -9,17 +11,28 @@ import { User } from "better-auth/types";
   ) {}
 
   async execute(
-    user:User,
-    id: string,
-    data: Partial<{ name: string; image: string }>
+    user:AppUser,
+    data: Partial<{ name: string; image: string;email: string }>,
+    id?: string,
   ): Promise<Partial<User>> {
 
-    if(user.id !== id) throw new Error("You can only update your own account");
+    if(isAdmin(user) && !id) throw new Error("User id is required");
 
-    const found = await this.userRepository.findById(id);
+    const target = isAdmin(user)?id:user.id
+
+    if(data.email){
+
+      const emailAlreadyExists = await this.userRepository.findByEmail(data.email);
+      if(emailAlreadyExists && emailAlreadyExists.id !== target) throw new Error("Email already exists");
+      if(emailAlreadyExists && emailAlreadyExists.id === target) throw new Error("You already have this email");
+
+    }
+
+
+    const found = await this.userRepository.findById(target!);
     if (!found) throw new Error("User not found");
 
-    const updatedUser = await this.userRepository.update(id, data);
+    const updatedUser = await this.userRepository.update(target!, data);
 
     return updatedUser;
   }
