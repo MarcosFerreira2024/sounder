0643df8b04ffb3fc0df3d7007c58d6b2a967ce26
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 03:13:31 -0300

    Register new repositories and services in DI container

--- backend/src/shared/container.ts
+++ backend/src/shared/container.ts
@@ -11,6 +11,24 @@ import { IMusicActionRepository } from "../modules/music/interfaces/IMusicAction
 import { MusicActionRepository } from "../modules/music/repos/MusicActionRepository";
 import { IAlbumRepository } from "../modules/album/interfaces/IAlbumRepository";
 import { AlbumRepository } from "../modules/album/repos/AlbumRepository";
+import { IDailyGameRepository } from "../modules/game/interfaces/IDailyGameRepository";
+import { IGameHistoryRepository } from "../modules/game/interfaces/IGameHistoryRepository";
+import { IGameSessionRepository } from "../modules/game/interfaces/IGameSessionRepository";
+import { IGameUserRepository } from "../modules/game/interfaces/IGameUserRepository";
+import { DailyGameRepository } from "../modules/game/repos/DailyGameRepository";
+import { GameHistoryRepository } from "../modules/game/repos/GameHistoryRepository";
+import { GameSessionRepository } from "../modules/game/repos/GameSessionRepository";
+import { GameUserRepository } from "../modules/game/repos/GameUserRepository";
+import { ArtistRepository } from "../modules/artist/repositories/ArtistRepository";
+import { IArtistRepository } from "../modules/artist/interfaces/IArtistRepository";
+import { IFileStorage } from "./storage/IFileStorage";
+import { DiskFileStorage } from "./storage/DiskFileStorage";
+import { IGameRepository } from "../modules/game/interfaces/IGameRepository";
+import { GameRepository } from "../modules/game/repos/GameRepository";
+import { ImageProcessingService } from "./services/ImageProcessingService";
+import { IImageProcessingService } from "./services/IImageProcessingService";
+import { IGameModesRepository } from "../modules/game/interfaces/IGameModesRepository";
+import { GameModesRepository } from "../modules/game/repos/GameModesRepository";
 
 container.registerSingleton<IUserRepository>(
   "UserRepository",
@@ -34,3 +52,24 @@ container.registerSingleton<IMusicRepository>("MusicRepository", MusicRepository
 container.registerSingleton<IMusicActionRepository>("MusicActionRepository", MusicActionRepository)
 
 container.registerSingleton<IAlbumRepository>("AlbumRepository", AlbumRepository)
+
+container.registerSingleton<IDailyGameRepository>("DailyGameRepository", DailyGameRepository)
+
+container.registerSingleton<IGameHistoryRepository>("GameHistoryRepository", GameHistoryRepository)
+
+container.registerSingleton<IGameSessionRepository>("GameSessionRepository", GameSessionRepository)
+
+container.registerSingleton<IGameUserRepository>("GameUserRepository", GameUserRepository)
+
+container.registerSingleton<IArtistRepository>("ArtistRepository", ArtistRepository)
+
+container.registerSingleton<IFileStorage>("FileStorage", DiskFileStorage)
+
+container.registerSingleton<IGameRepository>("GameRepository", GameRepository)
+
+container.registerSingleton<IGameModesRepository>("GameModesRepository", GameModesRepository)
+
+container.registerSingleton<IImageProcessingService>(
+  "ImageProcessingService",
+  ImageProcessingService
+)
