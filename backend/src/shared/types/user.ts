338d29d057274956b7fdd676ae9b5fb8e1b31730
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 03:14:54 -0300

    Update user.ts

--- backend/src/shared/types/user.ts
+++ backend/src/shared/types/user.ts
@@ -1,6 +1,9 @@
 import { User as BetterAuthUser } from "better-auth";
 import { UserRole } from "../../generated/prisma/enums";
+import { Artist } from "../../generated/prisma/client";
+import z from "zod"
 
 export type AppUser = BetterAuthUser & {
     role: UserRole;
+    artist?: Artist
 }
