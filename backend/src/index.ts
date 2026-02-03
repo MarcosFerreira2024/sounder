72652482e8ef5734c1fa80b37ff281415f226c65
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 02:56:35 -0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,13 @@ import "reflect-metadata";
 import "./shared/container";
 import { Server } from "./configs/server";
 import dotenv from "dotenv";
+import "./modules/game/cronjobs/dailyGame";
 
 dotenv.config();
 
 
 
 new Server(Number(process.env.PORT)).run()
+
+
+
