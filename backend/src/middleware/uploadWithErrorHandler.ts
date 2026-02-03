e234a9794adf512298a3a42009ce03a0f52d19fe
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-03 02:57:45 -0300

    Multer error handler

---
+++ backend/src/middleware/uploadWithErrorHandler.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import multer from "multer";
+import * as errorConstants from "../shared/constants/errors";
+import { handleAppError } from "../shared/helpers/handleAppError";
+
+export function uploadWithErrorHandler(uploadHandler: (req: Request, res: Response, next: NextFunction) => void) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        uploadHandler(req, res, (err: any) => {
+            if (err instanceof multer.MulterError) {
+                if (err.code === "LIMIT_UNEXPECTED_FILE") {
+                    return  handleAppError(res,new Error(errorConstants.Errors.FILE_REQUIRED(err.field || "File")));
+                }
+                return  handleAppError(res,err.message);
+            } else if (err) {
+                 if(err.message === "Multipart: Boundary not found"){
+                    return  handleAppError(res,new Error(errorConstants.Errors.FILE_REQUIRED("File")));
+                }
+                return  handleAppError(res,new Error((err.message)));
+            }
+            next();
+        });
+    };
+}
