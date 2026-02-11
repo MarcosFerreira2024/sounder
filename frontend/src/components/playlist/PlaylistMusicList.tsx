e46b2cb553c3bce6075fc174c099d5c61498ef39
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-11 04:55:38 -0300

    Update PlaylistMusicList.tsx

--- frontend/src/components/playlist/PlaylistMusicList.tsx
+++ frontend/src/components/playlist/PlaylistMusicList.tsx
@@ -1,14 +1,32 @@
-import MusicPreviewButton from "../ui/MusicPreviewButton";
+import { useAudioContext } from "../../contexts/AudioContext";
 import { usePlaylistContext } from "../../contexts/PlaylistContext";
+import type { Music } from "../../hooks/useAudio";
+import MusicPreviewButton from "../ui/MusicPreviewButton";
+import PlaylistMusicSkeleton from "./PlaylistMusicSkeleton";
 
 function PlaylistMusicList() {
-  const { musics } = usePlaylistContext();
+  const { musics, loading } = usePlaylistContext();
+
+  const { selectedSong } = useAudioContext();
+
+  if (!musics || loading)
+    return (
+      <div className="flex flex-col p-3 bg-neutral-950 border border-neutral-800 rounded-2xl h-full gap-4">
+        {Array.from({ length: 4 }).map((_, index) => (
+          <PlaylistMusicSkeleton index={index} key={index} />
+        ))}
+      </div>
+    );
 
   return (
     <div className="flex flex-col p-3 bg-neutral-950 border border-neutral-800 rounded-2xl h-full gap-4">
-      {musics.map((song) => (
-        <MusicPreviewButton key={song.id} song={song} />
-      ))}
+      {musics.map((music: Music) => {
+        const active = music.id === selectedSong?.id;
+
+        return (
+          <MusicPreviewButton key={music.id} data={music} selected={active} />
+        );
+      })}
     </div>
   );
 }
