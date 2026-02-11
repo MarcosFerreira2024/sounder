f040c0d994492ff0e70a3d44d3cf42198e68bad5
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-11 04:44:53 -0300

    Creating new buttons on sidebar / creating a signout option

--- frontend/src/components/ui/Sidebar.tsx
+++ frontend/src/components/ui/Sidebar.tsx
@@ -1,29 +1,55 @@
 import Button from "./Button";
-import { Home, Plus } from "lucide-react";
+import { Gamepad2, Home, LogOut, Plus } from "lucide-react";
 import CategoriesCarousel from "../CategoriesCarousel";
 import useVisibility from "../../hooks/useVisibility";
-import { ModalWrapper } from "./ModalWrapper";
 import AddPlaylistModal from "../playlist/AddPlaylistModal";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 function Sidebar() {
   const { close, isVisible, open } = useVisibility(false);
+  const { handleSignOut } = useAuth();
+  const navigate = useNavigate();
 
   return (
-    <div className="w-full h-full p-2 mt-2  bg-neutral-900 border border-neutral-800 rounded-2xl">
-      <div className="flex   items-center px-4 py-2.5 rounded-2xl  justify-between bg-neutral-950 border-neutral-900">
-        <div className="flex items-center gap-10">
-          <Button roundedValue="full" size="md" icon={<Home />} title="Home" />
-
+    <div className="w-full h-full p-2 mt-2 min-h-[94px] flex bg-neutral-900 border border-neutral-800 rounded-2xl">
+      <nav className="flex flex-1   items-center px-4 py-2.5 rounded-2xl  justify-between bg-neutral-950 border-neutral-900">
+        <div className="flex items-center gap-4 ">
+          <Button
+            onClick={handleSignOut}
+            roundedValue="full"
+            size="md"
+            icon={<LogOut />}
+            title="Add"
+          />
+          <Button
+            onClick={() => navigate("/")}
+            roundedValue="full"
+            size="md"
+            icon={<Home />}
+            title="Home"
+          />
           <CategoriesCarousel />
         </div>
-        <Button
-          onClick={() => open()}
-          roundedValue="full"
-          size="md"
-          icon={<Plus />}
-          title="Add"
-        />
-      </div>
+
+        <div className="flex gap-4">
+          <Button
+            onClick={() => navigate("/daily-game")}
+            roundedValue="full"
+            size="md"
+            icon={<Gamepad2 />}
+            title="Add"
+          />
+
+          <Button
+            onClick={() => open()}
+            roundedValue="full"
+            size="md"
+            icon={<Plus />}
+            title="Add"
+          />
+        </div>
+      </nav>
 
       {isVisible && <AddPlaylistModal onClose={close} />}
     </div>
