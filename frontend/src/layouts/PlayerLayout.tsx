e5dc5d4a9173a29152fd41b4f599c27d3b4557cf
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-11 04:29:44 -0300

    Removing audio provider

--- frontend/src/layouts/PlayerLayout.tsx
+++ frontend/src/layouts/PlayerLayout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { MusicCover } from "../components/MusicCover";
-import { AudioProvider } from "../contexts/AudioContext";
 
 function PlayerLayout({
   children,
@@ -14,19 +13,16 @@ function PlayerLayout({
   showExtraControls?: boolean;
 }) {
   return (
-    <AudioProvider>
-      <div className=" flex gap-2 ">
-        <MusicCover
-          isLyricsVisible={isLyricsVisible}
-          toggleLyricsVisibilty={toggleLyricsVisibilty}
-          showExtraControls={showExtraControls}
-        />
+    <div className=" flex gap-2 ">
+      <MusicCover
+        isLyricsVisible={isLyricsVisible}
+        toggleLyricsVisibilty={toggleLyricsVisibilty}
+        showExtraControls={showExtraControls}
+      />
 
-        {children}
-      </div>
-    </AudioProvider>
+      {children}
+    </div>
   );
 }
 
 export default PlayerLayout;
-
