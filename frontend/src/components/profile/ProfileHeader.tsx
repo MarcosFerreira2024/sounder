a683a7173b58e1d37469917393da139f9732d5b7
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-11 04:51:53 -0300

    Checking user permissions / adding follow button

--- frontend/src/components/profile/ProfileHeader.tsx
+++ frontend/src/components/profile/ProfileHeader.tsx
@@ -1,6 +1,9 @@
-import { MoreVertical } from "lucide-react";
+import { Heart, MoreVertical } from "lucide-react";
 import { MediaInfoHeader } from "../MediaInfoHeader";
 import Button from "../ui/Button";
+import { usePermissions } from "../../hooks/usePermissions";
+import { useParams } from "react-router-dom";
+import { useFollow } from "../../hooks/useFollow";
 
 interface ProfileHeaderProps {
   title: string;
@@ -15,9 +18,27 @@ export function ProfileHeader({
   image,
   showModal,
 }: ProfileHeaderProps) {
+  const { userId } = useParams();
+  const { isOwner, loading } = usePermissions();
+
+  const { isFollowingUser, toggleFollow, wait } = useFollow(userId);
+
+  if (loading) return null;
+
   return (
     <MediaInfoHeader subtitle={subtitle} title={title} image={image}>
-      <MoreVertical onClick={showModal} className="text-opacity" />
+      {isOwner(userId) ? (
+        <MoreVertical onClick={showModal} className="text-opacity" />
+      ) : (
+        <Button
+          onClick={toggleFollow}
+          disabled={wait}
+          icon={isFollowingUser ? <Heart fill="currentColor" /> : <Heart />}
+          roundedValue="full"
+          size="md"
+          className={wait ? "cursor-wait" : "cursor-pointer"}
+        />
+      )}
     </MediaInfoHeader>
   );
 }
