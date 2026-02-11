c54deb3883999c9d25e8c053a4213e47e630a706
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-11 04:49:57 -0300

    Update UserConnectionsSection.tsx

--- frontend/src/components/profile/UserConnectionsSection.tsx
+++ frontend/src/components/profile/UserConnectionsSection.tsx
@@ -1,39 +1,48 @@
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
-import Button from "../ui/Button";
 import { ProfileDisplayCardList } from "./ProfileDisplayCardList";
 
 interface UserConnectionsSectionProps {
-  data: { id: string; name: string; image: string }[];
+  data: { id: string; name: string; image: string }[] | null;
   type: "following" | "followers";
+  isLoading: boolean;
 }
 
 export function UserConnectionsSection({
   data,
   type,
+  isLoading,
 }: UserConnectionsSectionProps) {
-  const params = useParams();
-  const id = params.id;
+  const { userId } = useParams();
 
   const navigate = useNavigate();
+
+  if (!isLoading && (!data || data.length === 0)) return null;
 
   return (
     <div className="flex flex-col gap-2">
       <div className="flex justify-between gap-2">
-        <h1 className="text-main text-2xl">
+        <h1
+          className="text-main text-2xl"
+          onClick={() => navigate(`/profile/${userId}/${type}`)}
+        >
           {type === "followers" ? "Seguidores" : "Seguindo"}:
         </h1>
-        {data.length > 5 && (
-          <Button
-            onClick={() => navigate(`/profile/${id}/${type}`)}
-            className="text-opacity"
-            size="xs"
-            roundedValue="sm"
-          >
-            Ver mais
-          </Button>
-        )}
       </div>
-      <ProfileDisplayCardList profiles={data} />
+      {isLoading ||
+        (!data && (
+          <div className="flex gap-4 overflow-hidden">
+            {Array.from({ length: 6 }).map((_, index) => {
+              return (
+                <div
+                  key={index}
+                  aria-hidden
+                  className="h-[200px] min-h-[200px]  min-w-[200px] bg-neutral-900/80 rounded-2xl animate-pulse"
+                />
+              );
+            })}
+          </div>
+        ))}
+      <ProfileDisplayCardList isLoading={isLoading} profiles={data} />
     </div>
   );
 }
