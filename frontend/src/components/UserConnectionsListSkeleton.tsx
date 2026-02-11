ad62b301cd3caba4b2f322f7130f0fe7ab9b3bca
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-11 04:43:54 -0300

    Creating a skeleton to handle loading states of userConnectionsList

---
+++ frontend/src/components/UserConnectionsListSkeleton.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+type UserConnectionsListSkeletonProps = {
+  count?: number;
+};
+
+function UserConnectionsListSkeleton({
+  count = 9,
+}: UserConnectionsListSkeletonProps) {
+  return Array.from({ length: count }).map((_, index) => (
+    <div
+      key={index}
+      style={{ animation: "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite" }}
+      className="flex flex-col gap-2 items-center animate-pulse duration-100"
+    >
+      <div
+        aria-hidden="true"
+        className="  bg-neutral-900 min-w-70 min-h-70  rounded-full "
+      />
+      <div className="w-1/2 h-7.5 bg-neutral-900 rounded-2xl    " />
+    </div>
+  ));
+}
+
+export default UserConnectionsListSkeleton;
