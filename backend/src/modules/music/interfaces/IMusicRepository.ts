9da62a073b6d11d108b9bacf01d1a0702d042b18
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-11 04:06:48 -0300

    update getMusics return type and reformat repository interface

--- backend/src/modules/music/interfaces/IMusicRepository.ts
+++ backend/src/modules/music/interfaces/IMusicRepository.ts
@@ -1,45 +1,68 @@
 import { Music } from "../../../generated/prisma/client";
 
 export type musicQueryFilters = {
-    name?: string;
-    audio?: string;
-    id?: string;
-    artistId?: string;
-    authorName?: string;
+  name?: string;
+  audio?: string;
+  id?: string;
+  artistId?: string;
+  authorName?: string;
+};
 
-}
-
-export type MusicWithCover = Music & {album: {
-        cover: string;
-    } | null; }
-
-
+export type MusicWithCover = Music & {
+  album: {
+    cover: string;
+  } | null;
+};
 
 interface IMusicRepository {
-    getMusicById(musicId: string): Promise<Music | null>;
+  getMusicById(musicId: string): Promise<Music | null>;
 
-    getRandomMusic(exclude: string[]): Promise<MusicWithCover | null>;
+  getRandomMusic(exclude: string[]): Promise<MusicWithCover | null>;
 
-    deleteByAlbumId(albumId: string): Promise<void>;
+  deleteByAlbumId(albumId: string): Promise<void>;
 
-    updateMany(albumId: string, data: Partial<{ name: string; audio: string; artistId: string,albumId: string | null }>): Promise<void>
+  updateMany(
+    albumId: string,
+    data: Partial<{
+      name: string;
+      audio: string;
+      artistId: string;
+      albumId: string | null;
+    }>,
+  ): Promise<void>;
 
-    getMusics( search?: musicQueryFilters,page?: number, limit?: number): Promise<Music[]>;
+  getMusics(
+    search?: musicQueryFilters,
+    page?: number,
+    limit?: number,
+  ): Promise<MusicWithCover[]>;
 
-    createMusic(data: { name: string; audio: string; artistId: string, lyrics: string,genres:string[], albumId: string}): Promise<Music>;
+  createMusic(data: {
+    name: string;
+    audio: string;
+    artistId: string;
+    lyrics: string;
+    genres: string[];
+    albumId: string;
+  }): Promise<Music>;
 
-    addLike(musicId: string): Promise<void>;
+  addLike(musicId: string): Promise<void>;
 
-    removeLike(musicId: string): Promise<void>;
+  removeLike(musicId: string): Promise<void>;
 
+  updateMusic(
+    musicId: string,
+    data: Partial<{
+      name: string;
+      audio: string;
+      artistId: string;
+      albumId: string;
+    }>,
+  ): Promise<Music>;
 
-    updateMusic(musicId: string, data: Partial<{ name: string; audio: string; artistId: string,albumId: string }>): Promise<Music>;
+  deleteMusic(musicId: string): Promise<void>;
 
-    deleteMusic(musicId: string): Promise<void>;
-
-    assignMusicToAlbum(musicId:string,albumId:string):Promise<Music>
-
-
+  assignMusicToAlbum(musicId: string, albumId: string): Promise<Music>;
 }
 
 export { IMusicRepository };
