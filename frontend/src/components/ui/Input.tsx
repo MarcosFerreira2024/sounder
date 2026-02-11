d163fca7d589fe21f2fa84d70c7cbf635b08a515
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-11 04:47:00 -0300

    Creating an input component
    after some time i decided to create it 😂

---
+++ frontend/src/components/ui/Input.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+function Input({ ...props }: InputProps) {
+  return (
+    <input
+      className="placeholder:text-opacity w-full hover:bg-neutral-800 duration-200 ease-out text-main outline-none focus-within:ring-2 focus-within:ring-neutral-600 text-sm h-12.5 bg-neutral-900 border border-neutral-800 rounded-2xl shadow-md p-4"
+      {...props}
+    />
+  );
+}
+
+export default Input;
