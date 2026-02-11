851c9d19fd82165445aea3109e2bd32c98cf140e
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-11 04:28:58 -0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,27 +4,15 @@ import "./index.css";
 import App from "./App.tsx";
 import Tooltip from "./components/ui/Tooltip.tsx";
 import { TooltipProvider } from "./contexts/TooltipContext.tsx";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AppErrorProvider } from "./contexts/ErrorContext.tsx";
-import { UserProvider } from "./contexts/UserContext.tsx";
-
-// não sao perigosos de aparecer no repo ...
-export const GOOGLE_CLIENT_ID =
-  "1010171561666-t2q045q5d4hv0qj6nlm688phptesvte6.apps.googleusercontent.com";
-
-export const GITHUB_CLIENT_ID = "Ov23liqgHfsn806m6IO5";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppErrorProvider>
-      <UserProvider>
-        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-          <TooltipProvider>
-            <Tooltip />
-            <App />
-          </TooltipProvider>
-        </GoogleOAuthProvider>
-      </UserProvider>
+      <TooltipProvider>
+        <Tooltip />
+        <App />
+      </TooltipProvider>
     </AppErrorProvider>
-  </StrictMode>
+  </StrictMode>,
 );
