e7550d8be26b0b8885b3c5f9186f21bd5d761d7e
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-11 04:01:36 -0300

    Update ArtistRepository.ts

--- backend/src/modules/artist/repositories/ArtistRepository.ts
+++ backend/src/modules/artist/repositories/ArtistRepository.ts
@@ -1,77 +1,104 @@
 import { ArtistAccountStatus } from "../../../generated/prisma/enums";
 import { prisma } from "../../../libs/prismaClient";
-import { artistsQueryFilters, IArtistRepository } from "../interfaces/IArtistRepository";
+import {
+  artistsQueryFilters,
+  IArtistRepository,
+} from "../interfaces/IArtistRepository";
 
 class ArtistRepository implements IArtistRepository {
-    async assignUserAsArtist(userId: string): Promise<void> {
-        await prisma.artist.update({where: {userId}, data: {
-            status: "ACTIVE"
-        }})
-        
-    }
+  async assignUserAsArtist(userId: string): Promise<void> {
+    await prisma.artist.update({
+      where: { userId },
+      data: {
+        status: "ACTIVE",
+      },
+    });
+  }
 
-    async createArtist(userId: string): Promise<void> {
-        await prisma.artist.create({data: {userId}})
-    }
+  async createArtist(userId: string): Promise<void> {
+    await prisma.artist.create({ data: { userId } });
+  }
 
-    async getArtistById(artistId: string): Promise<{artistId:string; userId: string; name: string; status:ArtistAccountStatus } | null> {
-        const artist = await prisma.artist.findUnique({
-            where: {
-                id: artistId
-            },
-            select: {
-                id: true,
-                status: true,
-                user: {
-                    select: {
-                        name: true,
-                        id: true
-                    }
-                }
-            },
-        })
+  async getArtistById(artistId: string): Promise<{
+    artistId: string;
+    userId: string;
+    name: string;
+    status: ArtistAccountStatus;
+  } | null> {
+    const artist = await prisma.artist.findUnique({
+      where: {
+        id: artistId,
+      },
+      select: {
+        id: true,
+        status: true,
+        user: {
+          select: {
+            name: true,
+            id: true,
+          },
+        },
+      },
+    });
 
-        if(!artist) return null
+    if (!artist) return null;
 
-        return {
-            artistId: artist.id,
-            userId: artist.user.id,
-            name: artist.user.name,
-            status: artist.status,
-   
-        }
-    }
+    return {
+      artistId: artist.id,
+      userId: artist.user.id,
+      name: artist.user.name,
+      status: artist.status,
+    };
+  }
 
-    async getArtists( search?: artistsQueryFilters,page?: number, limit?: number): Promise<{artistId: string; userId: string; name: string; }[]> {
-        const artist = await prisma.artist.findMany({
-            where:{
-                ...(search?.name &&  { user: { name: { contains: search.name } } }),
-                ...(search?.id &&  { user: { id: search.id } }),
-                ...(search?.musicName &&  { music:{some:{name:{contains:search.musicName}}}}),
-                ...(search?.albumName &&  { albums:{some:{name:{contains:search.albumName}}}}),
-                ...(search?.id &&  { user: { id: search.id } }),
+  async getArtists(
+    search?: artistsQueryFilters,
+    page?: number,
+    limit?: number,
+  ): Promise<
+    {
+      artistId: string;
+      userId: string;
+      name: string;
+      image: string | null;
+      about: string | null;
+    }[]
+  > {
+    const artist = await prisma.artist.findMany({
+      where: {
+        ...(search?.name && { user: { name: { contains: search.name } } }),
+        ...(search?.id && { user: { id: search.id } }),
+        ...(search?.musicName && {
+          music: { some: { name: { contains: search.musicName } } },
+        }),
+        ...(search?.albumName && {
+          albums: { some: { name: { contains: search.albumName } } },
+        }),
+        ...(search?.id && { user: { id: search.id } }),
+      },
+      select: {
+        id: true,
+        user: {
+          select: {
+            name: true,
+            id: true,
+            about: true,
+            image: true,
+          },
+        },
+      },
+      skip: page && limit && (page - 1) * limit,
+      take: limit && limit,
+    });
 
-
-            },
-            select: {
-                id: true,
-                user: {
-                    select: {
-                        name: true,
-                        id: true
-                    }
-                }
-            },
-            skip: page && limit && (page - 1) * limit,
-            take: limit && limit  ,
-            }
-        )
-
-        return artist.map(artist => ({artistId: artist.id, userId: artist.user.id, name: artist.user.name }))
-
-
-
-    }
+    return artist.map((artist) => ({
+      artistId: artist.id,
+      userId: artist.user.id,
+      name: artist.user.name,
+      about: artist.user.about,
+      image: artist.user.image,
+    }));
+  }
 }
 
-export { ArtistRepository }
+export { ArtistRepository };
