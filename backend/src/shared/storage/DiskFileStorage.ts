1b98b651f0c23cf189b7a8f870429922345595cb
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-02-11 04:18:54 -0300

    Changing destination

--- backend/src/shared/storage/DiskFileStorage.ts
+++ backend/src/shared/storage/DiskFileStorage.ts
@@ -1,12 +1,12 @@
-import { promises as fs } from "fs"
-import path from "path"
-import crypto from "crypto"
-import { IFileStorage } from "./IFileStorage"
+import { promises as fs } from "fs";
+import path from "path";
+import crypto from "crypto";
+import { IFileStorage } from "./IFileStorage";
 
 export class DiskFileStorage implements IFileStorage {
   async save({ buffer, filename, folder }: any) {
-    const fileHash = crypto.randomBytes(16).toString("hex")
-    const safeName = `${fileHash}-${filename}`
+    const fileHash = crypto.randomBytes(16).toString("hex");
+    const safeName = `${fileHash}-${filename}`;
 
     const uploadPath = path.resolve(
       process.cwd(),
@@ -14,20 +14,21 @@ export class DiskFileStorage implements IFileStorage {
       "storage",
       "upload",
       "public",
-      folder
-    )
+      folder,
+    );
 
-    const destination = `src/storage/upload/public/${folder}`
+    const destination = `src/storage/upload/public/${folder}`;
 
+    const appURL = process.env.APP_URL;
 
-    await fs.mkdir(uploadPath, { recursive: true })
+    await fs.mkdir(uploadPath, { recursive: true });
 
-    const fullPath = path.join(uploadPath, safeName)
+    const fullPath = path.join(uploadPath, safeName);
 
-    await fs.writeFile(fullPath, buffer)
+    await fs.writeFile(fullPath, buffer);
 
     return {
-      path: destination+`/`+safeName
-    }
+      path: appURL + `/` + destination + `/` + safeName,
+    };
   }
 }
