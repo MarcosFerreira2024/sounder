f69bb3dd8cc3dfada3a84da46d7a90acea8a3cce
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-03-02 19:31:44 -0300

    Create LoaderContext.tsx

---
+++ frontend/src/contexts/LoaderContext.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, useState } from "react";
+import { GlobalLoader } from "../components/ui/GlobalLoader";
+
+interface LoaderContextType {
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const LoaderContext = createContext<LoaderContextType | undefined>(undefined);
+
+export const LoaderProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <LoaderContext.Provider value={{ loading, setLoading }}>
+      {loading && <GlobalLoader />}
+      {children}
+    </LoaderContext.Provider>
+  );
+};
+
+export const useLoader = () => {
+  const context = useContext(LoaderContext);
+  if (context === undefined) {
+    throw new Error("useLoader must be used within a LoaderProvider");
+  }
+  return context;
+};
