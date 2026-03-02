b4385c6517472f6fb5e048e27538cbbdf5df12f2
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-03-02 18:13:20 -0300

    Update prismaClient.ts

--- backend/src/libs/prismaClient.ts
+++ backend/src/libs/prismaClient.ts
@@ -1,9 +1,7 @@
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-import { PrismaLibSql } from "@prisma/adapter-libsql"
-import { PrismaClient } from "../generated/prisma/client"
-
-
-const adapter = new PrismaLibSql({
- url: "file:./db.db",
-})
-export const prisma = new PrismaClient({ adapter })
+const connectionString = process.env["DATABASE_URL"]!;
+const adapter = new PrismaPg({ connectionString });
+export const prisma = new PrismaClient({ adapter });
