8898791e7594aa5ce4ff4f4c1042877f8dd268d6
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-03-02 19:36:27 -0300

    Using React Query / why-did-you-render

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,38 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import Tooltip from "./components/ui/Tooltip.tsx";
 import { TooltipProvider } from "./contexts/TooltipContext.tsx";
-import { AppErrorProvider } from "./contexts/ErrorContext.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AppNotificationsProvider } from "./contexts/NotificationsContext.tsx";
+import { AuthProvider } from "./contexts/AuthContext.tsx";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
+
+if (true) {
+  const mod = await import("@welldone-software/why-did-you-render");
+  mod.default(React, {
+    trackAllPureComponents: true,
+  });
+}
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <AppErrorProvider>
-      <TooltipProvider>
-        <Tooltip />
-        <App />
-      </TooltipProvider>
-    </AppErrorProvider>
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <AppNotificationsProvider>
+        <TooltipProvider>
+          <Tooltip />
+          <App />
+        </TooltipProvider>
+      </AppNotificationsProvider>
+    </AuthProvider>
+  </QueryClientProvider>,
 );
