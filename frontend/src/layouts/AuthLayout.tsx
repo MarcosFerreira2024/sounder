d8a3ad7256f52bf2e35f715f2b0e2803bef61f30
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-03-02 19:34:27 -0300

    Update Auth/Main layouts and remove PlayerLayout

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AuthCarousel from "../components/auth/AuthCarousel";
 import { Link, useNavigate } from "react-router-dom";
+import Image from "../components/ui/Image";
 
 type AuthLayoutProps = {
   children: React.ReactNode;
@@ -22,23 +23,23 @@ function AuthLayout({
   const navigate = useNavigate();
 
   return (
-    <div className="bg-neutral-950 h-screen flex items-center  px-4 justify-center">
-      <img
+    <div className="bg-neutral-950 h-screen flex items-center py-2 px-2 md:py-4 md:px-4 justify-center">
+      <Image
         src="/logo.png"
         onClick={() => navigate("/")}
-        className="w-fit lg:hidden  absolute top-2 z-40 left-2 h-fit"
+        className=" lg:hidden w-30 absolute top-8 z-40 right-10 h-fit"
       />
       <div
         className="bg-neutral-900 shadow-md  border
-       border-neutral-800 w-[1440px] h-[800px] 
-       p-2 rounded-2xl "
+       border-neutral-800 w-[1440px] h-full lg:max-h-[800px] 
+        p-2 rounded-2xl "
       >
-        <div className="flex shadow-md  bg-neutral-950 border border-neutral-900 rounded-2xl h-full p-4 gap-6">
+        <div className="flex shadow-md  bg-neutral-950 border border-neutral-900 rounded-2xl h-full md:p-4 p-2 gap-6">
           <AuthCarousel isTyping={isTyping} />
 
           <div className="lg:w-2/3 w-full  flex flex-col gap-6">
             <div className="flex flex-col gap-2">
-              <h1 className="text-main text-4xl">{title}</h1>
+              <h1 className="text-main md:text-4xl text-2xl">{title}</h1>
               <p className="text-opacity text-sm">
                 {text}{" "}
                 <Link
--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -5,11 +5,9 @@ import Sidebar from "../components/ui/Sidebar";
 function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-neutral-950 h-screen">
-      <div className="mx-auto w-full flex flex-col h-[calc(100dvh - 16px)] pt-2 pb-2 px-4">
+      <div className="mx-auto w-full  flex flex-col h-[calc(100dvh - 16px)] lg:pt-2 pt-1 pb-1 px-2 lg:pb-2 lg:px-4">
         <Header />
-        <div style={{ minHeight: "calc(100dvh - 84px - 100px)" }}>
-          {children}
-        </div>
+        <div className="flex flex-col flex-1 ">{children}</div>
         <Sidebar />
       </div>
     </div>
