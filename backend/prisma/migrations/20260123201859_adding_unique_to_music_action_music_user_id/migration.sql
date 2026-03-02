45c8edb37e29463a288a773893b274f39c415cd7
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-03-02 18:10:25 -0300

    Removing sqlite migrations
    now the application uses postgres

--- backend/prisma/migrations/20260123201859_adding_unique_to_music_action_music_user_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[musicId,userId]` on the table `MusicAction` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "MusicAction_musicId_userId_key" ON "MusicAction"("musicId", "userId");
--- backend/prisma/migrations/20260128193242_adding_unique_on_user_game_status_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[gamemodeId,userId]` on the table `UserGameStatus` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "UserGameStatus_gamemodeId_userId_key" ON "UserGameStatus"("gamemodeId", "userId");
