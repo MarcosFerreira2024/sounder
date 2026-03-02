35e892c00995bf36ba72bdf999c9858ee3e7162b
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-03-02 19:36:58 -0300

    Add album, artist, game and music routes

--- frontend/src/consts/routes.ts
+++ frontend/src/consts/routes.ts
@@ -1,3 +1,4 @@
+import addToPlaylist from "../actions/music/removeFromPlaylist";
 import type { QueryType } from "../hooks/useSearch";
 
 export const BACKEND_API_URL = "http://localhost:3000/api";
@@ -20,7 +21,14 @@ export const routes = {
       return `${BACKEND_API_URL}/playlist/${playlistId}`;
     },
   },
-
+  album: {
+    getAlbumById(albumId: string) {
+      return `${BACKEND_API_URL}/albums/${albumId}`;
+    },
+    getMusicsByAlbumId(albumId: string) {
+      return `${BACKEND_API_URL}/albums/${albumId}/musics`;
+    },
+  },
   search: {
     searchByQuery(query: string, type: QueryType) {
       return `${BACKEND_API_URL}/search?q=${query}&type=${type}`;
@@ -53,15 +61,53 @@ export const routes = {
     getMusicsByPlaylistId(playlistId: string) {
       return `${BACKEND_API_URL}/playlist/${playlistId}/musics`;
     },
+    getById(musicId: string) {
+      return `${BACKEND_API_URL}/music/${musicId}`;
+    },
+    like(musicId: string) {
+      return `${BACKEND_API_URL}/music/${musicId}/like`;
+    },
+    removeFromPlaylist(musicId: string, playlistId: string) {
+      return `${BACKEND_API_URL}/playlist/${playlistId}/music/${musicId}`;
+    },
+    dislike(musicId: string) {
+      return `${BACKEND_API_URL}/music/${musicId}/dislike`;
+    },
+
+    addToPlaylist(musicId: string, playlistId: string) {
+      return `${BACKEND_API_URL}/playlist/${playlistId}/music/${musicId}`;
+    },
   },
 
   user: {
-    getUserInfo(userId?: string) {
+    getUserInfo(userId?: string | null) {
       return `${BACKEND_API_URL}/user${userId ? `?userId=${userId}` : ""}`;
     },
     updateUser() {
       return `${BACKEND_API_URL}/user`;
     },
     updateProfilePicture: `${BACKEND_API_URL}/user/profile-picture`,
+  },
+  artist: {
+    getArtists(query?: string) {
+      return `${BACKEND_API_URL}/artists${query ? `?${query}` : ""}`;
+    },
+  },
+
+  game: {
+    answer(mode: string) {
+      return `${BACKEND_API_URL}/game/answer?mode=${mode}`;
+    },
+    state(mode: string) {
+      return `${BACKEND_API_URL}/game/state?mode=${mode}`;
+    },
+    start(mode: string) {
+      return `${BACKEND_API_URL}/game/start?mode=${mode}`;
+    },
+    stats(mode: string) {
+      return `${BACKEND_API_URL}/game/user-stats?mode=${mode}`;
+    },
+    daily: `${BACKEND_API_URL}/game/daily-game`,
+    modes: `${BACKEND_API_URL}/game/modes`,
   },
 };
