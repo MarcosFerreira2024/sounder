cc19169c2017dc57889fc9770e064e31e278ee83
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-03-02 18:13:55 -0300

    Refactor multer handlers

--- backend/src/libs/multer.ts
+++ backend/src/libs/multer.ts
@@ -1,19 +1,29 @@
 import multer from "multer";
-
-
-
+import path from "node:path";
 
 const storage = multer.memoryStorage();
 
-export const upload = multer({
-    storage,
-    fileFilter: (req, file, cb) => {
-        if (!file.mimetype.includes("image")) {
-            return cb(new Error("Only images are allowed"));
-        }
-        cb(null, true);
+export const uploadImage = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+      return cb(null, true);
     }
 
+    return cb(new Error("Only image files are allowed"));
+  },
 });
+export const uploadLrc = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname).toLowerCase();
+    const isLrc =
+      ext === ".lrc" && file.mimetype === "application/octet-stream";
 
+    if (isLrc) {
+      return cb(null, true);
+    }
 
+    return cb(new Error("Only .lrc files are allowed"));
+  },
+});
