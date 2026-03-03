4a88d36732af6eaa38376e2017d00845ae1d66fb
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-03-03 19:48:40 -0300

    Using Vite proxy

--- frontend/src/consts/routes.ts
+++ frontend/src/consts/routes.ts
@@ -1,6 +1,6 @@
 import type { QueryType } from "../hooks/useSearch";
 
-export const BACKEND_API_URL = "https://sounder-idh8.vercel.app/api";
+export const BACKEND_API_URL = "/api";
 
 export const routes = {
   playlist: {
