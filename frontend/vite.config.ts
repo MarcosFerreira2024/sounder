ff02a1e1b4e8fd5a8436492ff96d027a06a04264
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-03-03 21:03:42 -0300

    Removing vite proxy (not needed)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,14 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://sounder-backend.vercel.app",
-        changeOrigin: true,
-        secure: true,
-        cookieDomainRewrite: "localhost",
-      },
-    },
-  },
 });
